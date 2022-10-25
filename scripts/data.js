4c3b07508ba70d22bfbc0f7f340443b4b064883f
Carlos Perez <cm.perez@uniandes.edu.co>
2022-10-24 22:07:14 -0500

    correcion data

--- scripts/data.js
+++ scripts/data.js
@@ -1,9 +1,20 @@
 import { Serie } from './serie.js';
 export var series = [
-    new Serie(1, "Rome", "Amazon", 2, "Historical drama depicting the period of history surrounding the violent transformation of the Roman Republic into the Roman Empire", "https://www.amazon.com/Rome-Season-1/dp/B003B63FKW", "https://i.imgur.com/1EwIO1C.jpeg"),
-    new Serie(2, "Quantum Leap", "Amazon", 5, "American science-fiction thriller set in the near future, where the future is a virtual reality and the past is a virtual reality", "https://www.amazon.com/Quantum-Leap-Volume-1/dp/B00170I7U2", "https://ringostrack.com/storage/covers/4534_quantum-leap.jpg"),
-    new Serie(3, "Brooklyn Nine-Nine", "Netflix", 8, "American police procedural comedy", "https://www.netflix.com/co-en/title/70281562", "https://m.media-amazon.com/images/M/MV5BNzVkYWY4NzYtMWFlZi00YzkwLThhZDItZjcxYTU4ZTMzMDZmXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_.jpg"),
-    new Serie(4, "The Big Bang Theory", "Amazon Prime Video", 12, "American comedy-drama about the lives of a group of college friends and their eccentric, often-bizarre, and socially awkward friends", "https://www.primevideo.com/region/na/detail/0I5NZJIND003YKEWNCSGGSZBIV/ref=atv_dp?language=es_ES", "https://i.blogs.es/4600c9/the-big-bang-theory/1366_2000.jpg"),
-    new Serie(5, "Rick And Morty", "Adult Swim", 5, "American animated science-fiction comedy", "https://www.adultswim.com/videos/rick-and-morty", "https://es.web.img3.acsta.net/pictures/18/10/31/17/34/2348073.jpg"),
-    new Serie(6, "House", "Amazon Prime Video", 8, "Follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg", "https://www.primevideo.com/detail/House/0Q387S9MM7GIJ0WFZEDTO59RFB", "https://m.media-amazon.com/images/M/MV5BMDA4NjQzN2ItZDhhNC00ZjVlLWFjNTgtMTEyNDQyOGNjMDE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"),
-];
+    new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
+    "https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg") ,
+    
+    new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", 
+    "https://www.netflix.com/co/title/70242311","https://i.imgur.com/EvKe48G.jpg"),
+    
+    new Serie (3, "Game of Thrones","HBO", 7, "American fantasy drama", "https://www.hbo.com/game-of-thrones",
+    "https://i.imgur.com/TDCEV1S.jpg"),
+
+    new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
+        "https://www.cbs.com/shows/big_bang_theory/about/", "https://i.imgur.com/uAEpVWk.jpg"),
+    
+    new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
+        "https://www.bbc.co.uk/programmes/b018ttws", "https://i.imgur.com/02B7qhj.jpg"),
+
+    new Serie (6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
+        "https://www.bbc.co.uk/programmes/p065smy4", "https://i.imgur.com/D4y3DrQ.jpg"),
+    ];
